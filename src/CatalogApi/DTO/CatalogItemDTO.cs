9ccc7965c7e1e80aad7fca04c7c310f5f73811b0
Iceblooms <iceblooms@gmail.com>
2023-12-06 15:22:28 +0600

    trying API
    Add request examples for the CatalogApi
    Rework CatalogItemDTO (because of deserialization error)

--- src/CatalogApi/DTO/CatalogItemDTO.cs
+++ src/CatalogApi/DTO/CatalogItemDTO.cs
@@ -1,7 +1,19 @@
-﻿namespace CatalogApi.DTO
+﻿using System.Text.Json.Serialization;
+
+namespace CatalogApi.DTO
 {
-    public record CatalogItemDTO(int Id, string? Name, string? Description)
+    public record CatalogItemDTO
     {
+        public int Id { get; init; }
+        public string? Name { get; init; }
+        public string? Description { get; init; }
+
+        [JsonConstructor]
+        public CatalogItemDTO(int id, string? name, string? description)
+        {
+            Id = id; Name = name; Description = description;
+        }
+
         public CatalogItemDTO(CatalogItem catalogItem)
             : this(catalogItem.Id, catalogItem.Name, catalogItem.Description)
         {
